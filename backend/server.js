753aa8ef861c5d596d372bdee26739bdd4d6df27
soumik7063 <ghataksoumik7063@gmail.com>
2025-12-03 15:25:42 +0530

    cors setup

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB();
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://jgec-gym-bros.vercel.app/'
+];
+
 app.use(cors({
-    origin: ['https://jgec-gym-bros.vercel.app/','http://localhost:5173/'],
-    credentials: true,
-}))
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); // allow non-browser requests like Postman
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 app.use(express.json());
 
 
